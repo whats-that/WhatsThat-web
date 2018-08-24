bce50f98c6ef4d51bc4f75fbd286f2b78ec5da48
Matt Krepp <mattkrepp@gmail.com>
2018-08-24 14:31:51 -0400

    updated seed

--- script/seed.js
+++ script/seed.js
@@ -1,69 +1,44 @@
 'use strict'
 
 const db = require('../server/db')
-const {User, Landmark, Thing} = require('../server/db/models')
-const {one, two} = require('./blobs')
-/**
- * Welcome to the seed file! This seed file uses a newer language feature called...
- *
- *                  -=-= ASYNC...AWAIT -=-=
- *
- * Async-await is a joy to use! Read more about it in the MDN docs:
- *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
- *
- * Now that you've got the main idea, check it out in practice below!
- */
+const {User, Landmark} = require('../server/db/models')
+const {james, customs, liberty,bull} = require('./blobs')
+
 async function seed() {
-  await db.sync({force: true})
-  console.log('db synced!')
-  const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'a@gmail.com', password: '12'}),
-    User.create({email: 'murphy@email.com', password: '123'}),
-    User.create({email: 'bb@gmail.com', password: '123'}),
-    User.create({email: 'cc@gmail.com', password: '123'}),
-    User.create({email: 'dd@gmail.com', password: '123'}),
-    User.create({email: 'ff@gmail.com', password: '123'}),
-    User.create({email: 'gg@gmail.com', password: '123'}),
-    User.create({email: 'hh@gmail.com', password: '123'}),
-    User.create({email: 'kk@gmail.com', password: '123'}),
-    User.create({email: 'rr@gmail.com', password: '123'}),
-    User.create({email: 'tt@gmail.com', password: '123'}),
-    User.create({email: 'qq@gmail.com', password: '123'}),
-    User.create({email: 'ww@gmail.com', password: '123'}),
-    User.create({email: 'ee@gmail.com', password: '123'}),
-    User.create({email: 'yy@gmail.com', password: '123'}),
-    User.create({email: 'uu@gmail.com', password: '123'}),
-    User.create({email: 'ii@gmail.com', password: '123'}),
-
-  ])
-  console.log(`seeded ${users.length} users`)
-  const landmarks = await Promise.all([
-    Landmark.create({name: 'one', rating: 4, image: one.base64, userId: 1}),
-    Landmark.create({name: 'two', image: two.base64, userId: 1})
-  ])
-
-  console.log(`seeded ${landmarks.length} landmarks`)
-  console.log(`seeded successfully`)
+ await db.sync({force: true})
+ console.log('db synced!')
+ const users = await Promise.all([
+  User.create({email: 'cody@email.com', password: '123'}),
+  User.create({email: 'a@gmail.com', password: '12'}),
+  User.create({email: 'murphy@email.com', password: '123'}),
+ ])
+ console.log(`seeded ${users.length} users`)
+ const landmarks = await Promise.all([
+  Landmark.create({name: "James Watson house", rating: 4, comment: 'The dissonance is quite interesting', image: james.base64, userId: 3}),
+  Landmark.create({name: "Alexander Hamilton U.S. Custom House", rating: 4, image: customs.base64, userId: 3}),
+  Landmark.create({name: "Statue of Liberty", rating: 3, comment: 'So overrated', image: liberty.base64, userId: 3}),
+  Landmark.create({name: "Charging Bull", rating: 5, image: bull.base64, userId: 3})
+ ])
+ console.log(`seeded ${landmarks.length} landmarks`)
+ console.log(`seeded successfully`)
 }
 
 async function runSeed() {
-  console.log('seeding...')
-  try {
-    await seed()
-  } catch (err) {
-    console.error(err)
-    process.exitCode = 1
-  } finally {
-    console.log('closing db connection')
-    await db.close()
-    console.log('db connection closed')
-  }
+ console.log('seeding...')
+ try {
+  await seed()
+ } catch (err) {
+  console.error(err)
+  process.exitCode = 1
+ } finally {
+  console.log('closing db connection')
+  await db.close()
+  console.log('db connection closed')
+ }
 }
 
 if (module === require.main) {
-  runSeed()
+ runSeed()
 }
 
-module.exports = seed
+module.exports = seed;
